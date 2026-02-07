0b1e4ca255523b95e3f8d742d02550ce7ddd9b50
Marco Colombara <colombaramarco21@gmail.com>
2026-02-07 11:58:41 +0100

    Changes

--- scratch/generateCards.js
+++ scratch/generateCards.js
@@ -1,16 +1,24 @@
 //Simple script to generate the json resources of the cards from the raw txt file
 const fs = require('fs');
 const path = require('path');
-const questionsFile = "questions.txt";
-const answersFile = "answers.txt";
+const questionsFile = "frasi.txt";
+const answersFile = "completamenti.txt";
 const generateJSON = (type) => {
-    const lines = fs.readFileSync(type, "utf8");
-    let result = [];
-    for(const line of lines) {
-        result.push([
-            line.trim(),
-            line.match("/_/g").length,
-            ""
-        ])
+    try {
+        const lines = fs.readFileSync(type, "utf8");
+        let array = [];
+        for(const line of lines) {
+            let string = line.trim();
+            string = string[0].toUpperCase() + string.slice(1);
+            array.push([
+                string,
+                line.match("/_/g").length,
+                type === questionsFile ? "frase" : "completamento"
+            ])
+        }
+        fs.writeFileSync(type.replace(".txt", ".json"), JSON.stringify(array));
+        return true;
+    } catch {
+        return false;
     }
 };
