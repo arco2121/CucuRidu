6ccc0b2aec09c9c99fcfe14a6623ab094195072c
Marco Colombara <colombaramarco21@gmail.com>
2026-02-08 12:27:24 +0100

    Mhe

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const { createServer } = require("node:http");
 const { Server } = require("socket.io");
 const express = require("express");
+const ejs = require('ejs');
 
 //Configuration
 const app = express();
@@ -24,12 +25,18 @@ app.use(express.json())
 app.get("/", (req, res) => {
     res.render("index");
 });
+app.get(['/home', '/index'], (req, res) => res.redirect('/'));
 
 server.on("connection", (user) => {
 
 });
 
 //Listening
+app.use((req, res) => res.status(104).render("error", {
+    error: 104,
+    message: "Questa pagina non esiste, brutta sottospecie di spermatozoo di elefante con la disfunzione erettile"
+}));
+
 app.listen(port, (error) => {
     console.log(`Server started on port ${port}`);
     if (error) {
