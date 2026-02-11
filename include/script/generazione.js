5bca1d34ec6ef3a2dd7748c7911cb747c5bdba0d
Abcia2 <105174769+Abcia2@users.noreply.github.com>
2026-02-11 17:47:25 +0100

    Script per generare nomi e dare pfp

--- include/script/generazione.js
+++ include/script/generazione.js
@@ -1,3 +1,4 @@
+const path = require("node:path");
 const alphabet = "QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm@#!£$%&/";
 
 const generateId = (length, memory) => {
@@ -21,3 +22,38 @@ const generateId = (length, memory) => {
 }
 
 module.exports = { generateId };
+
+
+// Generazione Nome Casuale
+const generateName = () => {
+    const fs = require('node:fs');
+    const path = require('node:path');
+
+    try {
+        const filePath = path.join(__dirname, '../names/names.json');
+        const rawData = fs.readFileSync(filePath, 'utf-8');
+        const data = JSON.parse(rawData);
+
+        //console.log(data.names);
+        //console.log(data.adjectives);
+
+        // Scelta random
+        let name = data.names[Math.floor(Math.random()*data.names.length)];
+        let adjective = data.adjectives[Math.floor(Math.random()*data.adjectives.length)];
+
+        let finalName = name + " " + adjective;
+        console.log(finalName);
+        return finalName;
+    } catch (error) {
+        console.error('Errore durante la lettura del file JSON:', error);
+    }
+}
+
+// Generazione Foto Profilo
+const generatePfp = () => {
+    const pfpNumber = 37;
+    const pfpPath = path.join('./assets/pfps/');
+
+    let rdmNumber = Math.floor(Math.random() * (pfpNumber + 1));
+    return pfpPath + rdmNumber + "jpg";
+}
