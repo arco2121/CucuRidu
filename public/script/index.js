a47c9ae905ed5c33463641d0f41b02949672f7ae
Marco Colombara <colombaramarco21@gmail.com>
2026-02-17 22:24:30 +0100

    Magari se lo rendi modulare dove server invece di essere stitico come un vecchio

--- public/script/index.js
+++ public/script/index.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const joinRoom = document.getElementById('joinRoom');
     const createRoom = document.getElementById('createRoom');
+    const optionsBtn = document.getElementById("optionsBtn");
 
     joinRoom.addEventListener("click", () => navigateWithLoading("/partecipaStanza"));
     createRoom.addEventListener("click", () => navigateWithLoading("/creaStanza"));
+    optionsBtn.addEventListener("click", () => document.dispatchEvent(showOptions));
 });
