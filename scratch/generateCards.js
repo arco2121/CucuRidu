131dd94be19534675c87ff1513c24fad0003b7be
Marco Colombara <colombaramarco21@gmail.com>
2026-02-19 12:49:12 +0100

    Ripreso lo script per la creazione di mazzi lato server

--- scratch/generateCards.js
+++ scratch/generateCards.js
@@ -3,38 +3,40 @@
 */
 const fs = require('node:fs');
 const {join} = require("node:path");
-const generateCards = (files) => {
+const generateCards = (group, files) => {
     try {
         for (const file of files) {
-            const lines = fs.readFileSync(join(__dirname, "/raw/cards/" + file), "utf-8")
+            const lines = fs.readFileSync(join(__dirname, "/raw/cards/" + group + "/" + file), "utf-8")
                 .split("\n").map(line => line.trim());
             let array = [];
             for (const line of lines) {
                 const string = line[0]?.toUpperCase() + line.slice(1);
-                const completamenti = (line.match(/_/g) || []).length;
-                array.push(completamenti > 0 ? [
+                const completamento = (line.match(/_/g) || []).length;
+                array.push([
                     string,
-                    completamenti
-                ] : string);
+                    completamento,
+                    completamento === 0 ? "completamento" : "frase"
+                ])
             }
-            fs.mkdirSync(join(__dirname, "../include/cards/"), {
+            fs.mkdirSync(join(__dirname, "../include/cards/" + group + "/"), {
                 recursive: true,
             });
-            fs.writeFileSync(join(__dirname, "../include/cards/" + file.replace(".txt", ".json")), JSON.stringify(array));
+            fs.writeFileSync(join(__dirname, "../include/cards/" + group + "/" + file.replace(".txt", ".json")), JSON.stringify(array));
         }
         return true;
-    } catch(e) {
-        console.error(e)
+    } catch (error) {
+        console.log(error)
         return false;
     }
 };
 
 const data = [];
-process.argv.slice(2).forEach(val => {
-    const input =  val.includes(".txt") ? val : null;
+process.argv.slice(2).forEach((val, index) => {
+    const input =  index === 0 ? (val || null) : (val.includes(".txt") ? val : null);
     data.push(input);
 });
-const files = [data[0] || "frasi.txt", data[1] || "completamenti.txt"];
+const groupName = data[0] || "standard";
+const files = [data[1] || "frasi.txt", data[2] || "completamenti.txt"];
 
-const result = generateCards(files);
+const result = generateCards(groupName, files);
 console.log("Result => " + result);
