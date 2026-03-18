c2cd2d95e3e13207c7eae996c151e78c92b0242d
Marco Colombara <colombaramarco21@gmail.com>
2026-03-18 09:33:37 +0100

    Caching

---
+++ public/script/serviceWorker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'v1_cache';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/assets/color.json',
+    '/style/global.css',
+    '/script/colorConfig.js',
+    '/script/ejs.js',
+    '/assets/icon.png',
+    '/assets/loading.webp'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
