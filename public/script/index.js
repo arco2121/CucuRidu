2e6f93f246ad1d3745490b7c0031b4844b3d0095
Marco Colombara <colombaramarco21@gmail.com>
2026-03-22 18:47:03 +0100

    Come giocare

--- public/script/index.js
+++ public/script/index.js
@@ -4,10 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const joinRoom = document.getElementById('joinRoom');
     const createRoom = document.getElementById('createRoom');
     const optionsBtn = document.getElementById("optionsBtn");
+    const rules = document.getElementById("rules");
     const sectionDefault = document.querySelector(".sectionToHide");
 
     joinRoom.addEventListener("click", () => navigateWithLoading("/partecipaStanza"));
     createRoom.addEventListener("click", () => navigateWithLoading("/creaStanza"));
+    rules.addEventListener("click", () => navigateWithLoading("https://github.com/arco2121/CucuRidu/wiki/Come-Giocare"));
     optionsBtn.addEventListener("click", () => {
         sectionDefault.dispatchEvent(hidePanel);
         optionPanel.dispatchEvent(showPanel);
