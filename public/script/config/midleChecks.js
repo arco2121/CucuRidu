7b443fc2d86f09d7545ca9db754c3b964604025d
Marco Colombara <colombaramarco21@gmail.com>
2026-07-25 11:46:09 +0200

    No syncCheck

--- public/script/config/midleChecks.js
+++ public/script/config/midleChecks.js
@@ -13,24 +13,20 @@ const checkHost = async (hosts) => {
     }
 };
 
-(async () => {
-    //await syncAll();
-    
-    const settings = JSON.parse(localStorage.getItem("cucuRiduSettings") || "{}");
-    if (fromBackEnd["deleteToken"] === true) {
-        settings["savingToken"] = null;
-        localStorage.setItem("cucuRiduSettings", JSON.stringify(settings));
-    }
+const settings = JSON.parse(localStorage.getItem("cucuRiduSettings") || "{}");
+if (fromBackEnd["deleteToken"] === true) {
+    settings["savingToken"] = null;
+    localStorage.setItem("cucuRiduSettings", JSON.stringify(settings));
+}
 
-    const token = settings.savingToken;
-    const params = new URLSearchParams(window.location.search);
-    if (!params.has("token") && token && fromBackEnd["loadToken"] !== false) {
-        const newUrl = new URL(window.location.href);
-        newUrl.searchParams.set("token", token);
-        window.location.replace(newUrl.href);
-    }
+const token = settings.savingToken;
+const params = new URLSearchParams(window.location.search);
+if (!params.has("token") && token && fromBackEnd["loadToken"] !== false) {
+    const newUrl = new URL(window.location.href);
+    newUrl.searchParams.set("token", token);
+    window.location.replace(newUrl.href);
+}
 
-    if(settings.translate) document.addEventListener("DOMContentLoaded", () => translateDom(null, lang));
-})();
+if(settings.translate) document.addEventListener("DOMContentLoaded", () => translateDom(null, lang));
 
 setInterval(() => checkHost(fromBackEnd["allowedOrigins"]), 20000);
